b157dc6714b488c7c68b2f3b2d225cd00f33c38b
Amber Roberts <astronomeramber@gmail.com>
2020-02-12 10:55:25 -0800

    Add files via upload

---
+++ Practical_Coding_Challenges/Connect_four.py
@@ -0,0 +1,31 @@
+
+"""
+Timestamp:
+Objective: Coding Connect-four from scratch
+Dependencies: 
+
+@author:
+///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+Let's see two examples!
+
+
+Connect-Four
+
+1/ BOARD
+
+User input grid size, start with a 4*4
+
+2/ DROP PIECE
+Place piece in board, alternating between players 1 and 2.
+Pieces will stack on top of each other so make sure you build from the bottom up.
+
+3/ WINNING MOVE
+The game should work for two players and tell you when you have won, be it horizontal vertical or diagonal (4 in a row).
+
+4/ MOVE IS VALID
+Check for excepts: "Illegal move", "This column is full.  Try another one!", "The board is full!"
+
+5/ 6x6 BOARD
+Still only win by getting 4 in a row (vertically, horizontally, diagonally)
+"""
